fe80830b221cbb566ded4ad67d58ef8edcced86f
Fatima-Rahmani79 <fatima.rahmani79@gmail.com>
2026-05-04 22:17:48 +0430

    Add icon to cards

--- src/components/story/Step2.jsx
+++ src/components/story/Step2.jsx
@@ -1,16 +1,78 @@
 import { motion } from "framer-motion";
+import { icons } from "lucide-react";
 
 const cards = [
-  { title: "Clean Layout", desc: "Simple & structured UI" },
-  { title: "User Focus", desc: "Designed for real users" },
-  { title: "Modern Style", desc: "Minimal & aesthetic" },
+  {
+    title: "Clean Layout",
+    desc: "Simple & structured UI",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="1em"
+        height="1em"
+        viewBox="0 0 24 24"
+      >
+        <title xmlns="">layout-outline</title>
+        <path
+          fill="currentColor"
+          fill-rule="evenodd"
+          d="M8.93 4.47a17.5 17.5 0 0 0-3.86 0a.666.666 0 0 0-.586.577a16.8 16.8 0 0 0 0 3.907a.666.666 0 0 0 .587.576c1.271.142 2.587.142 3.858 0a.666.666 0 0 0 .587-.576c.152-1.298.152-2.61 0-3.908a.666.666 0 0 0-.587-.576M4.903 2.98a19 19 0 0 1 4.192 0c.99.11 1.793.89 1.91 1.892a18.3 18.3 0 0 1 0 4.256a2.166 2.166 0 0 1-1.91 1.893c-1.382.154-2.81.154-4.192 0a2.166 2.166 0 0 1-1.91-1.893a18.3 18.3 0 0 1 0-4.256a2.166 2.166 0 0 1 1.91-1.892M8.93 14.47a17.5 17.5 0 0 0-3.86 0a.666.666 0 0 0-.586.576a16.8 16.8 0 0 0 0 3.908a.666.666 0 0 0 .587.576c1.271.142 2.587.142 3.858 0a.666.666 0 0 0 .587-.576c.152-1.298.152-2.61 0-3.908a.666.666 0 0 0-.587-.576m-4.026-1.49a19 19 0 0 1 4.192 0c.99.11 1.793.89 1.91 1.892a18.3 18.3 0 0 1 0 4.256a2.166 2.166 0 0 1-1.91 1.892c-1.382.155-2.81.155-4.192 0a2.166 2.166 0 0 1-1.91-1.892a18.3 18.3 0 0 1 0-4.256a2.166 2.166 0 0 1 1.91-1.892M18.93 4.47a17.5 17.5 0 0 0-3.86 0a.666.666 0 0 0-.586.577a16.8 16.8 0 0 0 0 3.907a.666.666 0 0 0 .587.576c1.271.142 2.587.142 3.858 0a.666.666 0 0 0 .587-.576c.152-1.298.152-2.61 0-3.908a.666.666 0 0 0-.587-.576m-4.026-1.49a19 19 0 0 1 4.192 0c.99.11 1.793.89 1.91 1.892a18.3 18.3 0 0 1 0 4.256a2.166 2.166 0 0 1-1.91 1.893c-1.382.154-2.81.154-4.192 0a2.166 2.166 0 0 1-1.91-1.893a18.3 18.3 0 0 1 0-4.256a2.166 2.166 0 0 1 1.91-1.892m4.027 11.49a17.5 17.5 0 0 0-3.86 0a.666.666 0 0 0-.586.576a16.8 16.8 0 0 0 0 3.908a.666.666 0 0 0 .587.576c1.271.142 2.587.142 3.858 0a.666.666 0 0 0 .587-.576c.152-1.298.152-2.61 0-3.908a.666.666 0 0 0-.587-.576m-4.026-1.49a19 19 0 0 1 4.192 0c.99.11 1.793.89 1.91 1.892a18.3 18.3 0 0 1 0 4.256a2.166 2.166 0 0 1-1.91 1.892c-1.382.155-2.81.155-4.192 0a2.166 2.166 0 0 1-1.91-1.892a18.3 18.3 0 0 1 0-4.256a2.166 2.166 0 0 1 1.91-1.892"
+          clip-rule="evenodd"
+        />
+      </svg>
+    ),
+  },
+  {
+    title: "User Focus",
+    desc: "Designed for real users",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="1em"
+        height="1em"
+        viewBox="0 0 24 24"
+      >
+        <title xmlns="">focus</title>
+        <g fill="none" stroke="currentColor" stroke-width="1.5">
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M20.325 8.3V6.45a2.775 2.775 0 0 0-2.775-2.775h-2.775m0 16.65h2.775a2.775 2.775 0 0 0 2.775-2.775V15.7m-16.65 0v1.85a2.775 2.775 0 0 0 2.775 2.775h2.775m0-16.65H6.45A2.775 2.775 0 0 0 3.675 6.45V8.3"
+          />
+          <path d="M12 8a4 4 0 1 1 0 8a4 4 0 0 1 0-8Z" />
+        </g>
+      </svg>
+    ),
+  },
+  {
+    title: "Modern Style",
+    desc: "Minimal & aesthetic",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="1em"
+        height="1em"
+        viewBox="0 0 24 24"
+      >
+        <title xmlns="">style</title>
+        <g fill="currentColor">
+          <path
+            fill-rule="evenodd"
+            d="M13 21v-8h8v8zm2-6h4v4h-4zM3 11V3h8v8zm2-6h4v4H5z"
+            clip-rule="evenodd"
+          />
+          <path d="M18 6v6h-2V8h-4V6zm-6 12H6v-6h2v4h4z" />
+        </g>
+      </svg>
+    ),
+  },
 ];
 
 export default function Step2() {
   return (
     <section className="min-h-screen flex flex-col items-center justify-center text-center bg-[#0B0B0F] text-white px-4 relative overflow-hidden">
       {/* Glow */}
-      <div className="absolute w-[400px] h-[400px] bg-indigo-500/20 blur-[120px] rounded-full" />
+      <div className="absolute w-[350px] h-[350px] bg-indigo-500/20 blur-[100px] rounded-full" />
 
       {/* 👇 subtle transition hint */}
       <motion.div
@@ -57,13 +119,14 @@ export default function Step2() {
             transition={{ delay: i * 0.2 }}
             viewport={{ once: true }}
             whileHover={{ y: -8 }}
-            className="group relative p-[1px] rounded-2xl bg-gradient-to-br from-indigo-500/20 via-transparent to-violet-500/20"
+            className="group relative p-[1px] rounded-2xl bg-white/5 backdrop-blur-xl via-transparent to-violet-500/20"
           >
             {/* glow on hover */}
-            <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition duration-500 bg-gradient-to-br from-indigo-500/30 to-violet-500/30 blur-xl rounded-2xl"></div>
+            <div className="absolute inset-0 opacity-0 group-hover:opacity-90 transition duration-300 bg-gradient-to-br from-blue-500/30 to-violet-400/30 blur-xl rounded-2xl"></div>
 
             {/* card content */}
-            <div className="relative bg-[#0B0B0F]/80 backdrop-blur-xl border border-white/10 rounded-2xl p-6 h-full transition duration-300 group-hover:border-indigo-400/40">
+            <div className="relative backdrop-blur-xl border border-white/10 rounded-2xl p-6 h-full transition duration-300 group-hover:border-indigo-400/40">
+              <p className="text-gray-400 text-lg ">{card.icon}</p>
               {/* title */}
               <h3 className="text-xl font-semibold mb-2">{card.title}</h3>
 
